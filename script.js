526f2ccba8ddcf773457eaf414f8f2ed8350f89f
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2025-07-09 17:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var isDate = function (input) {
   //   write your code here
+	if (input instanceof Date && !isNaN(input)) {
+    return true;
+  }
+  
+  if (typeof input === 'string' || typeof input === 'number') {
+    const parsedDate = new Date(input);
+    return !isNaN(parsedDate);
+  }
+  
+  // For all other cases
+  return false;
 };
 
 // Do not change the code below.
